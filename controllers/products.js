3724f3a2b0db8379b4d146589142593ce8e7791a
camerontorres <camerontorres759@gmail.com>
2023-03-26 23:42:27 -0400

    editing

--- controllers/products.js
+++ controllers/products.js
@@ -13,13 +13,13 @@ module.exports = {
     getProductPage: async (req, res) => {
       try{
         
-       let mongo = MongoClient.connect(DB_STRING, { useUnifiedTopology: true },function (err,client) {
+       let mongo = MongoClient.connect(DB_STRING, { useUnifiedTopology: true },function (err,client) { //connects to database
           if (err) throw err;
-         var db = client.db('test')
-           db.collection('products').findOne()
-          .then( result =>{
+         var db = client.db('test')                               //finds the database
+           db.collection('products').findOne()                   //finds document AND collection
+          .then( result =>{                                     //result is the found document
      
-      res.render("productPage.ejs",{product: result,}) 
+      res.render("productPage.ejs",{product: result,})        //result will be referenced in EJS as product
         })   
        })
       
