7faf36be8b5479a38c549145ad27608777f315a4
camerontorres <camerontorres759@gmail.com>
2023-03-27 15:46:26 -0400

    currently broken, attempting to get product page functional

--- controllers/products.js
+++ controllers/products.js
@@ -2,32 +2,43 @@
 
 
 const { MongoClient } = require('mongodb');
+const { db } = require('../models/product');
 const Products = require('../models/product')
 
 
 module.exports = {
   
     getProducts: (req, res) => {
-      res.render("products.ejs");
-    },
+      try{
+        const product = db.collection('products').find().toArray()
+        .then(result =>{
+      res.render("products.ejs", {product: result}) });
+    }  catch (err) {
+      console.log(err);}},
+
+
+
     getProductPage: async (req, res) => {
       try{
         
        let mongo = MongoClient.connect(DB_STRING, { useUnifiedTopology: true },function (err,client) { //connects to database
-          if (err) throw err;
-         var db = client.db('test')                               //finds the database
-           db.collection('products').findOne()                   //finds document AND collection
-          .then( result =>{                                     //result is the found document
-     
-      res.render("productPage.ejs",{product: result,})        //result will be referenced in EJS as product
+         if (err) throw err;
+        
+          const {id} = Products.findById(req.params.id)                
+          .then( result =>{                                            //result is the found document
+            res.send('id:' + req.params.id)
+      res.render("productPage",{product: result})                                             //result will be referenced in EJS as product
         })   
-       })
+        }) //mongoCL
       
-      }catch (err) {
-      console.log(err);
-    }
-  }  
+     }catch (err) {
+      console.log(err);}
+      }  
 
 
 
-}//mongod
+
+          //var db = client.db('test')          //finds the database
+          //const product = db.collection('products').find()   //finds document AND collection
+
+}//moduleEx
--- routes/products.js
+++ routes/products.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 const productsController = require("../controllers/products");
 
-//const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 
 router.get("/", productsController.getProducts);
 
 
-router.get("/productPage", productsController.getProductPage);
+router.get("/productPage/:id", ensureAuth, productsController.getProductPage);
 
 
 module.exports = router;
